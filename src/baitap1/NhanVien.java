38a6328be7ddea2213517f2cb7cf4e7b7e5fa0c6
chaucamly <ly.cc.59cntt@ntu.edu.vn>
2020-04-13 16:28:02 +0700

    v1.1

--- src/baitap1/NhanVien.java
+++ src/baitap1/NhanVien.java
@@ -5,11 +5,58 @@
  */
 package baitap1;
 
+
+
 /**
  *
  * @author Cam Ly
  */
-public class NhanVien {
+public class NhanVien 
+{
+    public String Ten;
+    public int  Tuoi;
+    public String  Diachi;
+    public double Tienluong;
+    public int Tongsogiolam;
+    NhanVien()
+    {
+        Ten="";
+        Tuoi=0;
+        Diachi="  ";
+        Tienluong=0;
+        Tongsogiolam=0;
+    }
     
-    
+    public NhanVien(String Ten, int Tuoi, String Diachi, double Tienluong, int Tongsogiolam)
+    {
+        this.Ten= Ten;
+        this.Tuoi= Tuoi;
+        this.Diachi= Diachi;
+        this.Tienluong= Tienluong;
+        this.Tongsogiolam= Tongsogiolam;
+    }
+    public String getThongTin() 
+    {
+        return "Ten: " + Ten + ", tuoi: " + Tuoi + ", diachi: " + Diachi+ ", tien luong: "+ Tienluong + ", tong gio lam: " + Tongsogiolam;
+    }
+    public double tinhThuong()
+    {
+        double thuong;
+        if(Tongsogiolam>=200)
+            thuong=Tienluong*0.2;
+        else if(Tongsogiolam>100 && Tongsogiolam<200)
+            thuong=Tienluong*0.1;
+        else
+            thuong=0;
+        return thuong;
+    }
+        
 }
+    
+    
+    
+    
+    
+    
+    
+
