d8b701942361fd1136e4677e7ec046af6402567c
chaucamly <ly.cc.59cntt@ntu.edu.vn>
2020-04-13 16:25:51 +0700

    v1.1

--- src/baitap1/BaiTap1Main.java
+++ src/baitap1/BaiTap1Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package baitap1;
+import java.util.Scanner;
 
 /**
  *
@@ -11,12 +12,53 @@ package baitap1;
  */
 public class BaiTap1Main 
 {
+    public static void main(String[] args)
+ {
+    Scanner nhap=new Scanner(System.in);
+    NhanVien nhanvien1=new NhanVien();
+    System.out.println("Ten nhan vien: ");
+    nhanvien1.Ten=nhap.nextLine();
+    System.out.println("Tuoi nhan vien: ");
+    nhanvien1.Tuoi=nhap.nextInt();
+    nhap.nextLine();
+    System.out.println("Dia chi nhan vien: ");
+    nhanvien1.Diachi=nhap.nextLine();
+    System.out.println("Tien luong nhan vien: ");
+    nhanvien1.Tienluong=nhap.nextDouble();
+    System.out.println("Tong so gio lam: ");
+    nhanvien1.Tongsogiolam=nhap.nextInt();
+    nhap.nextLine();
     
-    public static void main(String[] args) 
-    {
-            System.out.println("Bai Tap 1");
+    NhanVien nhanvien2=new NhanVien();
+    System.out.println("Ten nhan vien: ");
+    nhanvien2.Ten=nhap.nextLine();
+    System.out.println("Tuoi nhan vien: ");
+    nhanvien2.Tuoi=nhap.nextInt();
+    nhap.nextLine();
+    System.out.println("Dia chi nhan vien: ");
+    nhanvien2.Diachi=nhap.nextLine();
+    System.out.println("Tien luong nhan vien: ");
+    nhanvien2.Tienluong=nhap.nextDouble();
+    System.out.println("Tong so gio lam: ");
+    nhanvien2.Tongsogiolam=nhap.nextInt();
+    nhap.nextLine();
+    
+    System.out.println(nhanvien1.getThongTin());
+    System.out.println("Tien thuong: " + nhanvien1.tinhThuong());
+  
+    System.out.println(nhanvien2.getThongTin());
+    System.out.println("Tien thuong: " + nhanvien2.tinhThuong());
+    
+ }
+}
+    
+
+
+    
+    
+    
+            
             
  
-    }
-}
+    
 
